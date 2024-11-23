e1185260f71e6961f76c73441c09921c3efd8342
Rodrigo Ziviani de Souza <1624095+rzsouza@users.noreply.github.com>
2024-11-23 21:56:14 +1300

    add basic configuration from root directory

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Base configuration for all machines";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager } : {
+    homeManagerModules.base = import ./home/base.nix;
+  };
+}
