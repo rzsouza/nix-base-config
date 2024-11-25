70fe19eb62dec82024a769a1d18e014cfd89ddb2
Rodrigo Ziviani de Souza <1624095+rzsouza@users.noreply.github.com>
2024-11-25 23:46:18 +1300

    start

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Base configuration for all machines";
+  description = "Base development configurations";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -9,7 +9,48 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager } : {
-    homeManagerModules.base = import ./home/base.nix;
+  outputs = { self, nixpkgs, home-manager } : { 
+    homeManagerModules.base = { config, pkgs, ... }: {
+      home.packages = with pkgs; [
+        htop
+        killall
+        chromium
+        tldr
+        neovim
+        zsh
+      ];
+  
+      programs = {
+        git = {
+          enable = true;
+          
+          extraConfig = {
+            init.defaultBranch = "main";
+            pull.rebase = true;
+            rebase.autoStash = true;
+            core.editor = "nvim";
+            help.format = "html";
+            core.pager = "less --LONG-PROMPT --tabs=3 --quit-at-eof --quit-if-one-screen --tilde --jump-target=3 --ignore-case --status-column";
+          };
+        };
+  
+        chromium = {
+          enable = true;
+  
+          extensions = [
+            "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
+            "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
+            "nngceckbapebfimnlniiiahkandclblb" # Bitwarden
+            "bmanlajnpdncmhfkiccmbgeocgbncfln" # Slinky Elegant Theme
+          ];
+  
+          commandLineAgs = [
+            "--restore-last-session"
+            "--enable-spell-checking"
+            "--spelling-languages=en-NZ,en-GB,pt-BR"
+          ];
+        };
+      };  
+    };
   };
 }
