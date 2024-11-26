3a3877e86a3f08f98ca164c13fbd5627ffb7c5a2
Rodrigo Ziviani de Souza <1624095+rzsouza@users.noreply.github.com>
2024-11-26 20:04:40 +1300

    split flakes

---
+++ flakes/base-dev.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, ... }: {
+  home.packages = with pkgs; [
+    htop
+    killall
+    chromium
+    tldr
+    neovim
+    zsh
+  ];
+   
+  programs = {
+    git = {
+      enable = true;
+      
+      extraConfig = {
+        init.defaultBranch = "main";
+        pull.rebase = true;
+        rebase.autoStash = true;
+        core.editor = "nvim";
+        help.format = "html";
+        core.pager = "less --LONG-PROMPT --tabs=3 --quit-at-eof --quit-if-one-screen --tilde --jump-target=3 --ignore-case --status-column";
+      };
+    };
+    
+    chromium = {
+      enable = true;
+      
+      extensions = [
+        "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
+        "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
+        "nngceckbapebfimnlniiiahkandclblb" # Bitwarden
+        "bmanlajnpdncmhfkiccmbgeocgbncfln" # Slinky Elegant Theme
+      ];
+      
+      commandLineAgs = [
+        "--restore-last-session"
+        "--enable-spell-checking"
+        "--spelling-languages=en-NZ,en-GB,pt-BR"
+      ];
+    };
+  };  
+
+};
