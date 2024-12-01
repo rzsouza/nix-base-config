33db4b5324d168df4f938d9c3b2dee0d45288ba6
Rodrigo Ziviani de Souza <1624095+rzsouza@users.noreply.github.com>
2024-12-01 18:52:06 +1300

    Improve fzf configuration and add git aliases

--- flakes/base-dev.nix
+++ flakes/base-dev.nix
@@ -4,10 +4,8 @@
     killall
     tldr
     neovim
-    zsh
     lazygit
     ripgrep
-    fzf
     starship
   ];
    
@@ -29,9 +27,21 @@
       };
 
       shellAliases = {
+        gc = "git commit --verbose";
+        gd = "git diff";
+	gdca = "git diff --cached";
+	gf = "git fetch";
         gl = "git pull";
 	gp = "git push";
 	gst = "git status";
+	gm = "git merge";
+	gmom = "git merge origin/$(git_main_branch)";
+	gma = "git merge --abort";
+	glog = "git log --oneline --decorate --graph";
+	gpsup = "git push --set-upstream origin $(git_current_branch)";
+	gstl = "git stash list";
+	gstp = "git stash pop";
+	gsta = "git stash push";
 	vim = "nvim";
 	vi = "nvim";
 	nfu = "nix flake update";
@@ -56,7 +66,12 @@
       enable = true;
       enableZshIntegration = true;
     };
-    
+
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
     chromium = {
       enable = true;
       
